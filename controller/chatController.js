5115c789a023d0bbbfaf7dc2bab18ee2ad6dcbd2
daram <dmdgi37@naver.com>
2022-03-10 13:23:37 +0900

    유저 이름 띄워준 상태, 소스코드 바꾸기 전 백업

--- controller/chatController.js
+++ controller/chatController.js
@@ -19,10 +19,14 @@ export const getchatting = async (req, res) => {
     const user_id = _id;
     const friend_id = id;
     console.log(user_id,friend_id);
-    // const newChat = await Chat.create({
-    //     userid: user_id,
-    //     friendid: friend_id
-    // })
+    /*!!
+    아직 처리하기 전 상태라서 그렇지 주석 되어있는 코드 나중에 사용해야 할 코드임
+    여기에 if문으로 중복비교해주어서 userid랑 friendid가 중복되지 않으면 create해주어야함!!
+    const newChat = await Chat.create({
+        userid: user_id,
+        friendid: friend_id
+     })
+     */
 
     return res.status(400).render("chatting", {pageTitle: "chatting",friend_name,user_name});
 }
